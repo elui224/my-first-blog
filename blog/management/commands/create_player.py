c0eed8394a1a4089eae768708443614fc91c0255
Eric Lui <elui224@gmail.com>
2022-11-20 07:27:39 -0600

    Update create_player script

--- blog/management/commands/create_player.py
+++ blog/management/commands/create_player.py
@@ -5,15 +5,33 @@ from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from blog.models import Player, Team, Year
 
+from optparse import make_option
+
 CURRENT_FIFA_YEAR = date.today().year + 1
 
 class Command(BaseCommand):
+    '''
+    Run command using python manage.py create_player --file=path/to/myfile.csv.
+    For example, python manage.py create_player --file=./blog/file_import/players/drafted_players_2023.csv
+    '''
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('--file', type=str, help="file path")
+
     def handle(self, *args, **options):
-        with open(os.path.join(settings.BASE_DIR, f'blog/file_import/players/drafted_players_{CURRENT_FIFA_YEAR}.csv'), 'r') as csv_file:
+        file_path = options['file']
+        if file_path == None :
+            raise CommandError("Option `--file=...` must be specified.")
+
+        if not os.path.isfile(file_path) :
+            raise CommandError("File does not exist at the specified path.")
+
+        with open(file_path) as csv_file:
             csv_reader = csv.DictReader(csv_file, delimiter=',')
             for row in csv_reader:
                 try:
-                    Player.objects.create(
+                    p, created_player = Player.objects.get_or_create( #Use to avoid dupes. Get or replace returns tuple, indicating whether we're getting or creating based on inputs.
                         fifa_year=Year.objects.get(fifa_year=CURRENT_FIFA_YEAR),
                         team=Team.objects.get(manager_name=row['team']),
                         player_name=row['player_name'],     
@@ -21,4 +39,7 @@ class Command(BaseCommand):
                     )
                 except:
                     raise CommandError(f"Unable to add {row['player_name']} to {row['team']} for {CURRENT_FIFA_YEAR}")
-                self.stdout.write(self.style.SUCCESS(f"Successfully saved {row['player_name']} to {row['team']} for {CURRENT_FIFA_YEAR}!"))
+                if created_player:
+                    self.stdout.write(self.style.SUCCESS(f"Successfully saved {row['player_name']} to Team {row['team']} for {CURRENT_FIFA_YEAR}!"))
+                else:
+                    self.stdout.write(self.style.SUCCESS(f"Checked {row['player_name']} on Team {row['team']} for {CURRENT_FIFA_YEAR} and no changes are needed."))
